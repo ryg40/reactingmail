93a433dd0be8a70e96278928088950b60a3cfc31
ryg40 <ryg40@yahoo.com>
2018-06-04 19:46:35 -0400

    add prod build to index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-const bodyParser = require('body-parser');
-require('./models/User');
-require('./services/passport');
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
@@ -13,31 +13,29 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
-// if (process.env.NODE_ENV === 'production') {
-//     //express will serve up prod assets
+if (process.env.NODE_ENV === "production") {
+  //express will serve up prod assets
 
-//     app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-//     //Express will serve up the index.html 
-//         const path = require('path');
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
-//     });
-
-// };
-
+  //Express will serve up the index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
